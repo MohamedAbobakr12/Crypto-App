6b99cd9d3a54499a89c4872d4ac84198e59eb688
Dev.Mohamed <102235955+MohamedAbobakr12@users.noreply.github.com>
2025-04-08 03:31:54 +0200

    Update Features.jsx

--- src/app/sections/Features.jsx
+++ src/app/sections/Features.jsx
@@ -5,8 +5,8 @@ import Link from "next/link"
 const Features = () => {
     return (
         <section className="flex flex-col bg-black h-max" id="pages">
-            <div className="flex flex-col gap-y-20 max-w-5xl max-lg:max-w-max h-full lg:px-4 mx-auto mt-36 max-md:mt-18">
-                <div className="flex gap-x-20 max-xl:gap-x-10 max-lg:gap-x-8 w-full max-md:flex-col md:justify-around mx-auto">
+            <div className="flex flex-col gap-y-20 max-w-5xl lg:px-4 mx-auto mt-36 max-md:mt-18">
+                <div className="flex gap-x-20 max-xl:gap-x-10 max-lg:gap-x-8 max-md:flex-col md:justify-around mx-auto">
                     <div className="flex max-md:self-center max-md:items-center flex-col">
                         <h4 className="text-primary max-md:text-center font-semibold mt-16 max-md:mt-8">OUR FEATURE</h4>
                         <h1 className="text-white max-md:text-center text-4xl max-lg:text-3xl max-md:text-2xl max-xs:text-lg font-bold w-[30rem] max-xl:w-[28rem] max-lg:w-[24rem] max-md:w-[22rem] max-sm:w-[18rem] max-xs:w-[16rem] leading-14 max-lg:leading-10 max-md:leading-8 mx-auto mt-10 max-md:mt-6">Viewing long-term and short-term forecast</h1>
@@ -17,14 +17,13 @@ const Features = () => {
                 </div>
 
 
-
-                <div className="flex gap-x-20 max-xl:gap-x-16 w-full max-md:flex-col md:justify-around mx-auto px-5">
+                <div className="flex max-md:flex-col  gap-x-20 max-xl:gap-x-16 md:justify-around mx-auto xl:px-5">
                     <Image width={400} height={200} src={"/phone-2.svg"} alt={"phone image"} className={"max-md:self-center max-xl:w-96 max-lg:w-[300px] max-sm:w-[255px] max-xl:mt-10 max-md:order-1"} />
                     <div className="flex flex-col max-md:self-center max-lg:items-center max-lg:w-[25rem] max-md:w-[20rem]">
                         <h1 className="text-white max-lg:text-center text-4xl max-lg:text-2xl max-md:text-xl max-xs:text-lg font-bold w-[100%] max-lg:w-[20rem] max-sm:w-[18rem] max-xs:w-[16rem] leading-14 max-lg:leading-10 max-md:leading-8 mx-auto mt-16 max-md:mt-6">More than you think</h1>
-                        <p className="text-white max-lg:text-center font-medium max-lg:text-sm w-[25.5rem] max-lg:w-[21rem] max-md:w-[20rem] max-sm:w-[16rem] mt-8 max-md:mt-4">Add funds to your cryptocurrency account to start trading cryptocurrencies. You can add funds using different payment methods.</p>
+                        <p className="text-white max-md:text-center font-medium max-lg:text-sm w-[25.5rem] max-lg:w-[23rem] max-md:w-[22rem] max-sm:w-[19rem] mt-8 max-md:mt-4">Add funds to your cryptocurrency account to start trading cryptocurrencies. You can add funds using different payment methods.</p>
                         
-                        <div className="flex max-lg:flex-col max-lg:gap-y-6 max-md:gap-y-8 items-center w-[30rem] mt-10">
+                        <div className="flex max-lg:flex-col max-lg:gap-y-6 max-md:gap-y-8 items-center mt-10">
                             <div className="flex flex-col max-lg:justify-center">
                                 <div className="flex max-lg:justify-center max-lg:items-center gap-x-4">
                                     <Image src={"/productivity.svg"} width={20} height={20} alt={"productivity image"} />
